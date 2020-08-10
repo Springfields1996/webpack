8b392306de6144e9356dcc33cf3f315699579fdb
Bezzubov Artem <66363533+Springfields1996@users.noreply.github.com>
2020-08-10 20:29:01 +0300

    Hmework 10

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import './styles.css';
+import items from './menu.json';
+console.log(items);
+
+// const switcher = document.querySelector('#switcher-1');
+// const switcher2 = document.querySelector('#switcher-2');
+// const bodyClass = document.querySelector('body');
+// console.log(bodyClass);
+
+// switcher.addEventListener('click', () => {
+//   let darkTheme = switcher.checked;
+//   localStorage.setItem('theme', darkTheme);
+//   switcher2.checked = switcher.checked;
+
+//   localStorage.getItem('theme') === 'true'
+//     ? bodyClass.classList.add('background-black')
+//     : bodyClass.classList.remove('background-black');
+// });
+
+// switcher2.addEventListener('click', () => {
+//   let darkTheme = switcher2.checked;
+//   localStorage.setItem('theme', darkTheme);
+//   switcher.checked = switcher2.checked;
+
+//   localStorage.getItem('theme') === 'true'
+//     ? bodyClass.classList.add('background-black')
+//     : bodyClass.classList.remove('background-black');
+// });
+
+// localStorage.getItem('theme') === 'true'
+//   ? bodyClass.classList.add('background-black')
+//   : bodyClass.classList.remove('background-black');
