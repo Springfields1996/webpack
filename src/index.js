134b1188ee4e400a4e5f2b6dc1553a1779ee5666
Bezzubov Artem <66363533+Springfields1996@users.noreply.github.com>
2020-08-10 23:18:28 +0300

    Add theme switcher

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 import './styles.css';
 import items from './menu.json';
+console.log(items);
+
+const body = document.querySelector('body');
+const menuList = document.querySelector('.js-menu');
+const themeSwitch = document.querySelector('#theme-switch-control');
+const classes = body.classList;
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const themeSwitcher = event => {
+  if (event.target.checked) {
+    localStorage.setItem('theme', Theme.LIGHT);
+    classes.add(`${Theme.LIGHT}`);
+    classes.remove(`${Theme.DARK}`);
+  } else {
+    localStorage.setItem('theme', Theme.DARK);
+    classes.add(`${Theme.DARK}`);
+    classes.remove(`${Theme.LIGHT}`);
+  }
+};
+
+themeSwitch.addEventListener('click', themeSwitcher);
+
+localStorage.getItem('theme') === 'dark-theme'
+  ? body.classList.add(`${Theme.DARK}`)
+  : body.classList.add(`${Theme.LIGHT}`);
 
 // const switcher = document.querySelector('#switcher-1');
 // const switcher2 = document.querySelector('#switcher-2');
