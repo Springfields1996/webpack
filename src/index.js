8b3590aaad9f36814f6168ab192b5619cd135e1e
Bezzubov Artem <66363533+Springfields1996@users.noreply.github.com>
2020-08-13 11:19:19 +0300

    Add function "checkTheme"

--- src/index.js
+++ src/index.js
@@ -1,37 +1,43 @@
-import './styles.css';
-import items from './menu.json';
-import templateItem from './templates/menu-item.hbs';
+// import './styles.css';
+// import items from './menu.json';
+// import templateItem from './templates/menu-item.hbs';
 
-const body = document.querySelector('body');
-const menuList = document.querySelector('.js-menu');
-const themeSwitch = document.querySelector('#theme-switch-control');
-const classAdd = body.classList.add.bind(body.classList);
-const classRemove = body.classList.remove.bind(body.classList);
+// const body = document.querySelector('body');
+// const menuList = document.querySelector('.js-menu');
+// const themeSwitch = document.querySelector('#theme-switch-control');
+// const classAdd = body.classList.add.bind(body.classList);
+// const classRemove = body.classList.remove.bind(body.classList);
 
-menuList.insertAdjacentHTML('afterbegin', templateItem(items));
+// menuList.insertAdjacentHTML('afterbegin', templateItem(items));
 
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
+// const Theme = {
+//   LIGHT: 'light-theme',
+//   DARK: 'dark-theme',
+// };
 
-const themeSwitcher = event => {
-  if (!event.target.checked) {
-    localStorage.setItem('theme', Theme.LIGHT);
-    classAdd(Theme.LIGHT);
-    classRemove(Theme.DARK);
-  } else {
-    localStorage.setItem('theme', Theme.DARK);
-    classAdd(Theme.DARK);
-    classRemove(Theme.LIGHT);
-  }
-};
+// checkTheme();
 
-themeSwitch.addEventListener('change', themeSwitcher);
+// function checkTheme() {
+//   localStorage.getItem('theme') === Theme.DARK
+//     ? (classAdd(Theme.DARK), (themeSwitch.checked = true))
+//     : classAdd(Theme.LIGHT);
+// }
 
-localStorage.getItem('theme') === Theme.DARK
-  ? (classAdd(Theme.DARK), (themeSwitch.checked = true))
-  : classAdd(Theme.LIGHT);
+// const themeSwitcher = event => {
+//   if (!event.target.checked) {
+//     localStorage.setItem('theme', Theme.LIGHT);
+//     classAdd(Theme.LIGHT);
+//     classRemove(Theme.DARK);
+//   } else {
+//     localStorage.setItem('theme', Theme.DARK);
+//     classAdd(Theme.DARK);
+//     classRemove(Theme.LIGHT);
+//   }
+// };
+
+// themeSwitch.addEventListener('change', themeSwitcher);
+
+////////////////////////////////////////////////////// END Of HOMEWORK /////////////////////////////////////////////////
 
 // const switcher = document.querySelector('#switcher-1');
 // const switcher2 = document.querySelector('#switcher-2');
@@ -61,3 +67,143 @@ localStorage.getItem('theme') === Theme.DARK
 // localStorage.getItem('theme') === 'true'
 //   ? bodyClass.classList.add('background-black')
 //   : bodyClass.classList.remove('background-black');
+
+// console.log('Hello');
+
+// function toSayHello() {
+//   setTimeout(() => {
+//     console.log('Hi');
+//   }, 5000);
+// }
+
+// function toSayHelloInterval() {
+//   setInterval(() => {
+//     console.log('Hi');
+//   }, 4);
+// }
+
+// toSayHelloInterval();
+
+// let num = 0;
+
+// function toSayHello() {
+//   const int = setInterval(() => {
+//     console.log('VAsya');
+//     num++;
+//     if (num === 15) {
+//       clearInterval(int);
+//       return;
+//     }
+//   }, 1000);
+// }
+
+// // toSayHello();
+
+// const date = new Date();
+// // console.log(date);
+
+// const date2 = new Date(2202020000221);
+// // console.log(date2);
+
+// const date3 = Date.now(2020, 8, 1, 12, 43);
+// console.log(date3);
+
+// let num1 = Math.round(Math.random() * (10 - 1) + 1);
+
+// function fn(cl1, cl2) {
+//   if (num1 > 5) {
+//     cl1();
+//   } else {
+//     cl2();
+//   }
+// }
+
+// function sucess() {
+//   console.log('Ok');
+// }
+
+// function err() {
+//   console.log('Error');
+// }
+
+// fn(sucess, err);
+
+// function fn2() {
+//   let promise = new Promise((res, rej) => {
+//     res(console.log('Ok'));
+//     rej(console.log('Error'));
+//   });
+//   return promise;
+// }
+
+// // fn2();
+
+// let promise2 = new Promise((res, rej) => {
+//   setTimeout();
+//   res();
+//   rej();
+// });
+
+// let i = 0;
+// let counter = function () {
+//   setTimeout(counter, i);
+//   if (i <= 1400) {
+//     i += 100;
+//     if (i < 900) {
+//       console.log(`${i} So slow...`);
+//     } else if (i > 900 && i < 1200) {
+//       console.log(`${i} Wow, it's good speed!`);
+//     } else {
+//       console.log(`${i} No-no-no, faster, please`);
+//     }
+//   }
+// };
+
+// counter();
+
+// const result = document.querySelector('.count');
+// const startButton = document.querySelector('.button-start');
+// const stopButton = document.querySelector('.button-stop');
+// const lapButton = document.querySelector('.button-lap');
+// const lap = document.querySelector('.lap');
+
+// let timerId;
+// let counter = 0;
+// result.textContent = counter;
+// function letCount() {
+//   startButton.setAttribute('disabled', 'disabled');
+//   timerId = setInterval(() => {
+//     counter += 100;
+//     result.textContent = counter / 1000 + 's';
+//   }, 100);
+//   stopButton.textContent = 'Stop';
+// }
+
+// function stopCount() {
+//   clearInterval(timerId);
+//   startButton.removeAttribute('disabled');
+
+//   if (stopButton.textContent === 'Stop') {
+//     stopButton.textContent = 'Reset';
+//   } else if (stopButton.textContent === 'Reset') {
+//     counter = 0;
+//     result.textContent = counter;
+//     lap.remove();
+//   }
+// }
+
+// function setLap() {
+//   if (stopButton.textContent === 'Reset') {
+//     lap.setAttribute('disabled', 'disabled');
+//   } else {
+//     lap.removeAttribute('disabled');
+//     const text = `<p>${counter / 1000} </p>`;
+//     lap.innerHTML += text;
+//   }
+// }
+
+// startButton.addEventListener('click', letCount);
+
+// stopButton.addEventListener('click', stopCount);
+
+// lapButton.addEventListener('click', setLap);
